98dc602681600a0e3848f74bc530913205da5791
Muyleang Ing <116934056+MuyleangIng@users.noreply.github.com>
2024-03-27 20:10:51 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         <code> wellome to devops</code> and save to reload.
+         <code> wellome to devops</code> from webhook
         </p>
         <a
           className="App-link"
